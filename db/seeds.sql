74b74c064632d9e7b3e3f707a4e2403759dc6dcb
cpharshaw <cpharshaw+git@gmail.com>
2019-03-02 00:12:10 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lunch burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dinner burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dessert burger', false);
+
